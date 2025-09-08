49a4188b83c5ae1b77096762c594e63e7fec2df5
aditya singh <adisingh142002@gmail.com>
2025-09-08 09:46:48 +0530

    edited

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -71,6 +71,19 @@ export const MobileMenu = ({menuOpen, setMenuOpen}) => {
                                 `}
                             > Contact 
                              </a> 
+                              <a 
+                                 href="#/adityamain.pdf" download="adityamain.pdf"
+                                 onClick={() => setMenuOpen(false)}
+                                 className={`text-2xl font-semibold text-white my-4 transform transition-transform duration-300
+                                    ${
+                                         menuOpen
+                                          ? "opacity-100 translate-y-0" 
+                                          :"opacity-0 translate-y-5"
+
+                                    }
+                                `}
+                            > Resume
+                             </a> 
     </div> 
     );
 };
